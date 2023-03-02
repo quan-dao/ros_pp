2f0fde8e8c41da53a3a56f516ffa00784f5166ef
mqdao2018 <minh-quan.dao@ls2n.fr>
2023-03-02 14:16:56 +0000

    rename torch to ros_pp

---
+++ ros_pp/models/backbones_2d/map_to_bev/pointpillar_scatter.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+class PointPillarScatter(nn.Module):
+    def __init__(self, model_cfg, grid_size, **kwargs):
+        super().__init__()
+
+        self.model_cfg = model_cfg
+        self.num_bev_features = self.model_cfg.NUM_BEV_FEATURES
+        self.nx, self.ny, self.nz = grid_size
+        assert self.nz == 1
+
+    def forward(self, coords: torch.Tensor, pillar_features: torch.Tensor, batch_size: int = 1) -> torch.Tensor:
+        batch_spatial_features = []
+        for batch_idx in range(batch_size):
+            spatial_feature = pillar_features.new_zeros(self.num_bev_features, self.nz * self.nx * self.ny)
+
+            batch_mask = coords[:, 0] == batch_idx
+            this_coords = coords[batch_mask, :]
+            indices = this_coords[:, 1] + this_coords[:, 2] * self.nx + this_coords[:, 3]
+            indices = indices.type(torch.long)
+            pillars = pillar_features[batch_mask, :]
+            pillars = pillars.t()
+            spatial_feature[:, indices] = pillars
+            batch_spatial_features.append(spatial_feature)
+
+        batch_spatial_features = torch.stack(batch_spatial_features, 0)
+        batch_spatial_features = batch_spatial_features.view(batch_size, self.num_bev_features * self.nz, self.ny, self.nx)
+        return batch_spatial_features
