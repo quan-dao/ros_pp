a20fc2db68ef7a0bba11560b1b73c2d50b37463a
CBeaune <charlotte.beaune@eleves.ec-nantes.fr>
2023-03-21 11:08:46 +0100

    Edit launch file- add editable parameters for tf-publisher

---
+++ ros_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/user/Documents/ros_pp/ros_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/user/Documents/ros_pp/ros_ws/build'
+export PYTHONPATH='/opt/ros/noetic/lib/python3/dist-packages:/opt/ros/foxy/lib/python3.8/site-packages:/home/user/Documents/CARLA/PythonAPI/carla/:/home/user/Documents/CARLA/scenario_runner:/home/user/Documents/CARLA/leaderboard:/home/user/Documents/CARLA/PythonAPI/carla/dist/carla-0.9.13-py3.7-linux-x86_64.egg'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/user/Documents/ros_pp/ros_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/user/Documents/ros_pp/ros_ws/src:$ROS_PACKAGE_PATH"
---
+++ ros_ws/build/v2x_tracking/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Documents/ros_pp/ros_ws/src/v2x_tracking
+# Build directory: /home/user/Documents/ros_pp/ros_ws/build/v2x_tracking
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
